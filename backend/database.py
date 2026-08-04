c23edcd97b44853f8bfa4b15ec12247c257c8efc
alberto <alberto.spezzapria@gmail.com>
2026-08-04 16:42:21 +0200

    integrating correct components and requirements

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # to modify when using docker
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost:5432/interviewdb"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg://myuser:mypassword@localhost:5433/interviewdb"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
